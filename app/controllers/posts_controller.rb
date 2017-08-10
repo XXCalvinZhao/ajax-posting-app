dac14012905cbd475f26a35413c9a1bb7b5632de
Calvin <xcalvin.zhao@gmail.com>
2017-08-10 22:35:29 +0800

    2-6 Ajax删除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,8 +16,6 @@ class PostsController < ApplicationController
    def destroy
      @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
      @post.destroy
-
-     redirect_to posts_path
    end
 
    protected
